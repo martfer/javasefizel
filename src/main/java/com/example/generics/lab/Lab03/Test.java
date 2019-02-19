3cbaf258b2749fe892c54c938c42e3431f70a868
Martin Feri <ferenc.martin@dpc.hu>
2019-02-19 16:03:28 +0100

    added lab03

--- src/main/java/com/example/generics/lab/Lab03/Test.java
+++ src/main/java/com/example/generics/lab/Lab03/Test.java
@@ -1,7 +1,7 @@
 package com.example.generics.lab.Lab03;
 
 
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -13,23 +13,27 @@ public class Test {
         List<Orange> oranges = List.of(new Orange(1), new Orange(10));
         List<Fruit> fruits = List.of(new Apple(1), new Orange(10));
 
-        System.out.println("Collections.max(apples).equals(new Apple(10)) = " + Test.max(apples).equals(new Apple(10)));
-        System.out.println("Collections.max(oranges).equals(new Orange(10)) = " + Test.max(oranges).equals(new Orange(10)));// compile-time error
+        System.out.println("max(apples).equals(new Apple(10)) = " + max(apples).equals(new Apple(10)));
+        System.out.println("max(oranges).equals(new Orange(10)) = " + max(oranges).equals(new Orange(10)));
         // compile-time error
-//        System.out.println("Collections.max(fruits).equals(new Orange(10)) = " + max(fruits).equals(new Orange(10)));
-
-        System.out.println(Test.max(apples));
-        System.out.println(Test.max(oranges));
-        // compile-time error
-//        System.out.println(Comparators.max(fruits));
-//        System.out.println(Comparators.<Fruit>max(apples));
+//        System.out.println("max(fruits).equals(new Orange(10)) = " + max(fruits).equals(new Orange(10)));
+//        System.out.println(max(fruits));
+//        System.out.println(Test.<Fruit>max(apples));
 
         Apple weeApple = new Apple(1);
         Apple bigApple = new Apple(2);
         apples = List.of(weeApple, bigApple);
         System.out.println("weeApple.compareTo(bigApple) = " + weeApple.compareTo(bigApple));
-        System.out.println("Collections.max(apples) == bigApple = " + (Collections.max(apples) == bigApple));
+        System.out.println("max(apples) == bigApple = " + (max(apples) == bigApple));
     }
 
-    //max
+    public static <T extends Comparable<T>> T max(Collection<T> coll) {
+        T candidate = null;
+        for (T elt : coll) {
+            if (candidate == null || candidate.compareTo(elt) < 0) {
+                candidate = elt;
+            }
+        }
+        return candidate;
+    }
 }
