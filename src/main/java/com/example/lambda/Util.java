2979691c4493571b448d7a8d4e2ce07517c88adb
Martin Feri <ferenc.martin@dpc.hu>
2019-02-26 13:34:00 +0100

    mapToInt example

--- src/main/java/com/example/lambda/Util.java
+++ src/main/java/com/example/lambda/Util.java
@@ -3,13 +3,6 @@ package com.example.lambda;
 public class Util {
     // A generic static method
     public static <U> int[] mapToInt(U[] list, Mapper<? super U> mapper) {
-        int[] mappedValues = new int[list.length];
-
-        for (int i = 0; i < list.length; i++) {
-            // Map the object to an int
-            mappedValues[i] = mapper.map(list[i]);
-        }
-
-        return mappedValues;
+        return null;
     }
 }
